22f1377bf85d808df4ae6ae9167bed242f3c7102
Benjamin Bjoern Kroeger <benjamin_kroeger@t-online.de>
2025-11-29 05:11:35 +0100

    ADD progress bar

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ def train(
 
 
         val_losses = []
-        for x, y in val_dl:
+        for x, y in tqdm(val_dl,deci=f"Validating epoch {epoch}"):
             val_loss = val_step(x, y, model, device, precision)
             val_losses.append(val_loss)
 
